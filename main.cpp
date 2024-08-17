eb971e28d6f6acadf31cc44aa93f17f41772f3da
pi55man <cuinnv@gmail.com>
2024-08-17 16:55:38 +0530

    working server client

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <cstring>
 #include <iostream>
 #include <netinet/in.h>
-#include <stdio.h>
 #include <endian.h>
 #include<arpa/inet.h>
 #include <cstdlib>
@@ -14,9 +13,25 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <netinet/ip.h>
+
 using namespace std;
 
-static void dummy(int connfd);
+static void dummy(int connfd){
+	char rbuf[64]=  {};
+	ssize_t n = read(connfd, rbuf, sizeof(rbuf)-1);
+	cout<<"n: "<<n;
+	if(n<0){
+	printf("read: %s \n",rbuf);
+		return;
+	} else if(n==0){
+	cout<<"client dc";
+	}
+	cout.flush();
+	cout<<"client says: \n"<<rbuf<<flush;
+	
+	char wbuf[] = "world";
+	write(connfd, wbuf, strlen(wbuf));
+}
 
 int main() {
   	int fd = socket(AF_INET,SOCK_STREAM,0);
@@ -26,10 +41,6 @@ int main() {
 	//to set socket options after creating the socket, use setsockopt()
 	int val = 1;
 	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
-	struct timeval tv;
-	tv.tv_sec = 5;
-	tv.tv_usec = 0;
-	setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO,&tv, sizeof(tv));
 	//SOL_SOCKET specifies that so_reuseaddr is a socket layer option, not a protocol layer one.
 	//SO_REUSEADDR  allows a socket to bind an address and port which is already in use, but prev socket
 	//has not fully been released. without it, bind() will fail when you restart the server.
@@ -38,8 +49,8 @@ int main() {
 	//ntohs and ntohl convert the numbers in req big endian formats.
 	struct sockaddr_in addr= {};
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(1234);
-	addr.sin_addr.s_addr = htonl(0);
+	addr.sin_port = ntohs(1234);
+	addr.sin_addr.s_addr = ntohl(0);
 	int rv = bind(fd, (struct sockaddr*)&addr, sizeof(addr));
 	if(rv){
 	cout<<"bind()";
@@ -56,8 +67,8 @@ int main() {
 	//accept connections
 	while(true){
 	struct sockaddr_in client_addr ={};
-	socklen_t addrlen = sizeof(client_addr);
-	int connfd = accept(fd, (struct sockaddr* )&client_addr, &addrlen);
+	socklen_t socklen = sizeof(client_addr);
+	int connfd = accept(fd, (struct sockaddr* )&client_addr, &socklen);
 	//the accept syscall also returns the peers address, addrlen is both input and output.
 	
 	if(connfd<0){continue;}
@@ -66,22 +77,4 @@ int main() {
 	}
 	
 	return 0;
-}
-
-
-static void dummy(int connfd){
-	char rbuf[64]=  {};
-	ssize_t n = read(connfd, rbuf, sizeof(rbuf)-1);
-	
-	if(n<0){
-	cout<<"read() error";
-	return;
-	} else if(n==0){
-	cout<<"client dc";
-	}
-	rbuf[n] = '\0';
-	cout<<"client says: \n"<<rbuf;
-
-	char wbuf[] = "world";
-	write(connfd, wbuf, strlen(wbuf));
 }
