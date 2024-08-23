a6c361e060d6e36b5d5c862b7561e1b1e7d3b08c
pi55man <cuinnv@gmail.com>
2024-08-23 22:36:48 +0530

    todo debug

--- client.cpp
+++ client.cpp
@@ -6,7 +6,7 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <endian.h>
-#include<arpa/inet.h>
+#include <arpa/inet.h>
 #include <cstdlib>
 #include <unistd.h>
 #include <sys/types.h>
@@ -22,26 +22,37 @@ using namespace std;
 const size_t k_max_msg = 4096;
 static int32_t write_all(int fd, const char* buf, size_t n);
 static int32_t read_full(int fd, char* buf, size_t n);
-static int32_t query(int fd, const char* text){
-	uint32_t  len = (uint32_t)strlen(text);
-	
+
+static int32_t send_req(int fd, const char* text){
+	uint32_t len = (uint32_t)strlen(text);
+	if(len>k_max_msg){return -1;}
+
 	char wbuf[4+k_max_msg];
-	memcpy(wbuf, &len, 4);
-	memcpy(&wbuf[4],text, len);
-	write_all(fd, wbuf, 4+len);
-
-	char rbuf[4+k_max_msg+1];
-	read_full(fd, rbuf, 4);
-
-	memcpy(&len, rbuf, 4);
-	int32_t err;
-	err = read_full(fd, &rbuf[4],len);
-	if(err){cout<<"read() error"; return err;}
-	rbuf[4+len]='\0';
-	printf("server says: %s\n",&rbuf[4]);
-	return 0; 
+	memcpy(wbuf,&len,4);
+	memcpy(&wbuf[4],text,len);
+	return write_all(fd,wbuf,4+len);
 }
 
+static int32_t read_res(int fd){
+
+	char rbuf[4+k_max_msg+1];
+	errno = 0;
+	int32_t err = read_full(fd,rbuf,4);
+	if(err){
+		if(errno == 0){cout<<"eof"<<flush;}
+		else{cout<<"read() error"<<flush;}
+		return err;
+	}
+	uint32_t len = 0;
+	memcpy(&len,rbuf,4);
+	if(len>k_max_msg){cout<<"too long"<<flush;}
+	err = read_full(fd, &rbuf[4], len);
+
+	rbuf[4+len] = '\0';
+	printf("server says: %s\n",&rbuf[4]);
+	return 0;
+
+}
 	
 int main(){
 	int fd = socket(AF_INET, SOCK_STREAM,0);
@@ -51,12 +62,24 @@ int main(){
 	addr.sin_port=ntohs(1234);
 	addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);
 	int rv = connect(fd, (const struct sockaddr*)&addr, sizeof(addr));
-	int32_t err = query(fd,"hello");
-	if(err){close(fd);}
-	err = query(fd,"hello2");
-	if(err){close(fd);}
-	err =query(fd,"hello3");
-	if(err){close(fd);}
+
+	const char* query_list[3] = {"hello1","hello2","hello3"};
+	for(size_t i=0;i<3;++i){
+		int32_t err = send_req(fd,query_list[i]);
+		if(err){
+			goto L_DONE;
+		}
+	}
+	for(size_t i = 0;i<3;++i){
+		int32_t err = read_res(fd);
+		if(err){
+			goto L_DONE;
+		}
+	}
+	L_DONE:
+		close(fd);
+		return 0;
+
 }
 
 static int32_t write_all(int fd, const char* buf, size_t n){
